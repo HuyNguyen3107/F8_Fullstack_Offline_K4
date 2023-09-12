eea451e62f106c98d780be4cda844d6f8650a6c8
Manh Huy <levi2k3ds@gmail.com>
2023-09-12 22:58:37 +0700

    Nop bai tap buoi 24

--- Day_24/ex01.js
+++ Day_24/ex01.js
@@ -74,27 +74,54 @@ function resetEdit(e, taskName) {
     changeTasks.forEach(function (changeTask) {
       changeTask.addEventListener("click", function (e) {
         var check = changeTask.parentElement.previousElementSibling;
-        console.log(value);
         e.target.parentElement.parentElement.outerHTML = `
         <div class="todos-main" style="margin-top: 32px;">
                     <input type="text" id="task" class="value-change" value="${value}" placeholder="Update Task">
                     <button class="btn-add-task change-task">Add Task</button>
                 </div>
         `;
-        var completeChange = document.querySelector(".change-task");
-        var valueChange = document.querySelector(".value-change");
-        valueChange.addEventListener("keyup", function (e) {
-          if (e.key === "Enter") {
+        var completeChangeList = document.querySelectorAll(".change-task");
+        var valueChangeList = document.querySelectorAll(".value-change");
+        valueChangeList.forEach(function (valueChange) {
+          valueChange.addEventListener("keyup", function (e) {
+            if (e.key === "Enter") {
+              console.log("huy");
+              var exchange = valueChange.value.replaceAll("<", "&lt;");
+              e.target.parentElement.outerHTML = `
+            <div class = "todos-task">
+            <span class="task-name check${++count}">${exchange}</span>
+            <div class="edit-icon">
+                <i class="fa-solid fa-pen-to-square change"></i>
+                <i class="fa-solid fa-trash close"></i>
+            </div>
+            </div>
+            `;
+              if (check.style.opacity !== "") {
+                var checkList = document.querySelectorAll(".task-name");
+                checkList.forEach(function (checkItem) {
+                  if (checkItem.classList.contains(`check${count}`)) {
+                    checkItem.style.opacity = "0.4";
+                    checkItem.style.textDecoration = "line-through";
+                  }
+                });
+              }
+              resetEdit(event, exchange);
+            }
+          });
+        });
+        completeChangeList.forEach(function (completeChange) {
+          completeChange.addEventListener("click", function (e) {
+            e.preventDefault();
             var exchange = valueChange.value.replaceAll("<", "&lt;");
             e.target.parentElement.outerHTML = `
-          <div class = "todos-task">
-          <span class="task-name check${++count}">${exchange}</span>
-          <div class="edit-icon">
-              <i class="fa-solid fa-pen-to-square change"></i>
-              <i class="fa-solid fa-trash close"></i>
-          </div>
-          </div>
-          `;
+            <div class = "todos-task">
+            <span class="task-name check${count}">${exchange}</span>
+            <div class="edit-icon">
+                <i class="fa-solid fa-pen-to-square change"></i>
+                <i class="fa-solid fa-trash close"></i>
+            </div>
+            </div>
+            `;
             if (check.style.opacity !== "") {
               var checkList = document.querySelectorAll(".task-name");
               checkList.forEach(function (checkItem) {
@@ -105,30 +132,7 @@ function resetEdit(e, taskName) {
               });
             }
             resetEdit(event, exchange);
-          }
-        });
-        completeChange.addEventListener("click", function (e) {
-          e.preventDefault();
-          var exchange = valueChange.value.replaceAll("<", "&lt;");
-          e.target.parentElement.outerHTML = `
-          <div class = "todos-task">
-          <span class="task-name check${count}">${exchange}</span>
-          <div class="edit-icon">
-              <i class="fa-solid fa-pen-to-square change"></i>
-              <i class="fa-solid fa-trash close"></i>
-          </div>
-          </div>
-          `;
-          if (check.style.opacity !== "") {
-            var checkList = document.querySelectorAll(".task-name");
-            checkList.forEach(function (checkItem) {
-              if (checkItem.classList.contains(`check${count}`)) {
-                checkItem.style.opacity = "0.4";
-                checkItem.style.textDecoration = "line-through";
-              }
-            });
-          }
-          resetEdit(event, exchange);
+          });
         });
       });
     });
