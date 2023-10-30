9660041bf1d1fbfbdaa45e3d3fbf69dcbb56e683
Manh Huy <levi2k3ds@gmail.com>
2023-10-31 01:14:00 +0700

    Datepicker

--- Day_37/js/blog.js
+++ Day_37/js/blog.js
@@ -23,14 +23,14 @@ const handleProgress = function () {
         handleProgress();
       }, 1000);
     }
-  }, 500);
+  }, 1000);
   if (time === 0) {
     setTimeout(function () {
       time = 100;
       alertDiv.style.transform = "";
       progress.style.width = "";
       return;
-    }, 500);
+    }, 1000);
   }
 };
 
@@ -76,8 +76,40 @@ const blog = {
                             placeholder="Please enter your content" style="resize: none;"></textarea>
                     </div>
                     <div class="time-field">
-                        <label for="time-post">Set time to post!</label>
-                        <input type="text" placeholder="Can't use now" id="time-post" required disabled>
+                    <label for="time-post">Set time to post!</label>
+                    <input type="text" placeholder="MM/DD/YYYY" id="time-post" disabled>
+                    <i class="fa-regular fa-calendar"></i>
+                    <div class="calendar-container">
+                        <div class="wrapper">
+                            <div class="calendar-header">
+                                <div class="present">
+                                <p class="current-date"></p>
+                                <i class="fa-solid fa-sort-down"></i>
+                                </div>
+                                <div class="icons">
+                                    <span id="prev">
+                                        <i class="fa-solid fa-chevron-left"></i>
+                                    </span>
+                                    <span id="next">
+                                        <i class="fa-solid fa-chevron-right"></i>
+                                    </span>
+                                </div>
+                                <ul class="years"></ul>
+                            </div>
+                            <div class="calendar">
+                                <ul class="weeks">
+                                    <li>Sun</li>
+                                    <li>Mon</li>
+                                    <li>Tue</li>
+                                    <li>Wed</li>
+                                    <li>Thu</li>
+                                    <li>Fri</li>
+                                    <li>Sat</li>
+                                </ul>
+                                <ul class="days"></ul>
+                            </div>
+                        </div>
+                    </div>
                     </div>
                     <button class="btn-post-new">Write new!</button>
                 </form>
@@ -90,17 +122,18 @@ const blog = {
           blog.renderPosts();
           blog.handlePost();
           blog.handleLogout();
+          blog.handleCalender();
         }
       });
     } else {
       loading.style.display = "";
       let html;
       html = `
-        <div class="page-start">
-        <h1 class="heading">Blogger</h1>
-        <button class="sign-in">Sign in</button>
-    </div>
-        `;
+          <div class="page-start">
+          <h1 class="heading">Blogger</h1>
+          <button class="sign-in">Sign in</button>
+      </div>
+          `;
       this.root.innerHTML = html;
       this.signIn();
       blog.renderPosts();
@@ -480,7 +513,7 @@ const blog = {
               <div class="view-detail-post"><a href="#"># view more test...</a></div>
               <div class="post-footer">
                   <div class="user-profile"><a href="#"># admin01</a></div>
-                  <div class="read-time">Khoảng 1 giây đọc</div>
+                  <div class="read-time">Khoảng ${blog.randomNum()} giây đọc</div>
               </div>
           </div>
       </div>
@@ -524,7 +557,7 @@ const blog = {
             <div class="view-detail-post"><a href="#"># view more test...</a></div>
             <div class="post-footer">
                 <div class="user-profile"><a href="#"># admin01</a></div>
-                <div class="read-time">Khoảng 1 giây đọc</div>
+                <div class="read-time">Khoảng ${blog.randomNum()} giây đọc</div>
             </div>
         </div>
     </div>
@@ -533,6 +566,7 @@ const blog = {
     postsEl.prepend(postEl);
   },
   handlePost: async function () {
+    const currentTime = new Date().getTime();
     const btnPostNew = document.querySelector(".btn-post-new");
     btnPostNew.addEventListener("click", async function (e) {
       e.preventDefault();
@@ -545,32 +579,50 @@ const blog = {
       const title = titleEl.value;
       const content = contentEl.value;
       if (title && content) {
-        const { response, data: post } = await client.post("/blogs", {
-          title,
-          content,
-        });
-        titleEl.value = "";
-        contentEl.value = "";
-        if (response.ok) {
+        if (!document.querySelector("#time-post").value) {
+          const { response, data: post } = await client.post("/blogs", {
+            title,
+            content,
+          });
+          titleEl.value = "";
+          contentEl.value = "";
+          if (response.ok) {
+            btnPostNew.disabled = false;
+            btnPostNew.style.cursor = "";
+            btnPostNew.style.transform = "";
+            blog.renderPost(post.data.userId.name, title, content);
+            blog.alertSuccess("Thêm bài viết thành công");
+          } else {
+            blog.handleRefreshToken().then(async () => {
+              const { response, data: post } = await client.post("/blogs", {
+                title,
+                content,
+              });
+              if (response.ok) {
+                btnPostNew.disabled = false;
+                btnPostNew.style.cursor = "";
+                btnPostNew.style.transform = "";
+                blog.renderPost(post.data.userId.name, title, content);
+                blog.alertSuccess("Thêm bài viết thành công");
+              }
+            });
+          }
+        } else {
           btnPostNew.disabled = false;
           btnPostNew.style.cursor = "";
           btnPostNew.style.transform = "";
-          blog.renderPost(post.data.userId.name, title, content);
-          blog.alertSuccess("Thêm bài viết thành công");
-        } else {
-          blog.handleRefreshToken().then(async () => {
-            const { response, data: post } = await client.post("/blogs", {
-              title,
-              content,
-            });
-            if (response.ok) {
-              btnPostNew.disabled = false;
-              btnPostNew.style.cursor = "";
-              btnPostNew.style.transform = "";
-              blog.renderPost(post.data.userId.name, title, content);
-              blog.alertSuccess("Thêm bài viết thành công");
-            }
-          });
+          let time = document.querySelector("#time-post").value.split("/");
+          let timeSet = new Date(`${time[2]}-${time[0]}-${time[1]}`).getTime();
+          if (timeSet <= currentTime) {
+            blog.alertError("Vui lòng chọn lại thời gian đăng bài!");
+          } else {
+            titleEl.value = "";
+            contentEl.value = "";
+            document.querySelector("#time-post").value = "";  //`Bài viết của bạn sẽ được đăng vào ${}` `${time[2]}-${time[0]}-${time[1]}`
+            blog.alertSuccess(
+              `Bài viết của bạn sẽ được đăng vào ${blog.formatDate(`${time[2]}-${time[0]}-${time[1]}`)}`
+            );
+          }
         }
       } else {
         btnPostNew.disabled = false;
@@ -617,6 +669,248 @@ const blog = {
       });
     }
   },
+  randomNum: function () {
+    const num = Math.floor(Math.random() * 10) + 1;
+    return num;
+  },
+  handleCalender: function () {
+    const calendarContainer = document.querySelector(".calendar-container");
+    const calendarIcon = document.querySelector(".fa-calendar");
+    calendarIcon.addEventListener("click", function (e) {
+      e.stopPropagation();
+      if (calendarContainer.style.display === "") {
+        calendarContainer.style.display = "block";
+      } else {
+        calendarContainer.style.display = "";
+      }
+    });
+    calendarContainer.addEventListener("click", function (e) {
+      e.stopPropagation();
+    });
+    document.addEventListener("click", function () {
+      calendarContainer.style.display = "";
+    });
+    const timePost = document.querySelector("#time-post");
+    const daysTag = document.querySelector(".days");
+    const currentDate = document.querySelector(".current-date");
+    const listIcon = document.querySelectorAll(".icons span");
+    const years = document.querySelector(".years");
+    const present = document.querySelector(".present");
+    const icons = document.querySelector(".icons");
+    const arrow = document.querySelector(".present i");
+    // getting new date, current year and month
+    let date = new Date();
+    let currYear = date.getFullYear();
+    let currMonth = date.getMonth();
+    let currDate = date.getDate();
+    let checkMonth = currMonth;
+    let checkYear = currYear;
+
+    // storing full name of all months in array
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const renderCalendar = () => {
+      let firstDayofMonth = new Date(currYear, currMonth, 1).getDay();
+      let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
+      let lastDayofMonth = new Date(
+        currYear,
+        currMonth,
+        lastDateofMonth
+      ).getDay();
+      let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+      let liTag = "";
+
+      for (let i = firstDayofMonth; i > 0; i--) {
+        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+      }
+      for (let i = 1; i <= lastDateofMonth; i++) {
+        let isToday =
+          i === +currDate &&
+          +currYear === +checkYear &&
+          +currMonth === +checkMonth
+            ? "active"
+            : "";
+        // console.log(isToday);
+        // console.log(isToday);
+        liTag += `<li class="${isToday}">${i}</li>`;
+      }
+
+      for (let i = lastDayofMonth; i < 6; i++) {
+        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+      }
+      currentDate.innerText = `${months[currMonth]} ${currYear}`;
+      daysTag.innerHTML = liTag;
+    };
+    renderCalendar();
+    listIcon.forEach((icon) => {
+      icon.addEventListener("click", (e) => {
+        e.stopPropagation();
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+
+        if (currMonth < 0 || currMonth > 11) {
+          date = new Date(currYear, currMonth);
+          // console.log(date);
+          currYear = date.getFullYear();
+          currMonth = date.getMonth();
+        } else {
+          date = new Date(currYear, currMonth);
+          // console.log(date);
+        }
+        // console.log(date);
+        renderCalendar();
+        const dayList = document.querySelectorAll(".days li");
+        dayList.forEach(function (item) {
+          item.addEventListener("click", (e) => {
+            e.stopPropagation();
+            e.target.classList.add("active");
+            currDate = e.target.innerText;
+            checkMonth = currMonth;
+            checkYear = currYear;
+            let monthShow;
+            if (e.target.classList.contains("inactive")) {
+              monthShow =
+                Array.from(dayList).indexOf(e.target) > 20
+                  ? currMonth + 1
+                  : currMonth - 1;
+            } else {
+              monthShow = currMonth;
+            }
+            timePost.value = `${
+              +(monthShow + 1) < 10 ? "0" + (monthShow + 1) : monthShow + 1
+            }/${+currDate < 10 ? "0" + currDate : currDate}/${currYear}`;
+            dayList.forEach(function (item) {
+              if (item.classList.contains("active") && item !== e.target) {
+                item.classList.remove("active");
+              }
+            });
+          });
+        });
+      });
+    });
+    let yearLiList = "";
+    for (let i = 1970; i <= 2300; i++) {
+      let isYear = i === currYear ? "selected" : "";
+      yearLiList += `<li class="${isYear}">${i}</li>`;
+    }
+    years.innerHTML = yearLiList;
+    present.addEventListener("click", function (e) {
+      e.stopPropagation();
+      if (years.style.display === "") {
+        years.style.display = "flex";
+        icons.style.display = "none";
+        arrow.style.transform = "rotate(180deg)";
+      } else {
+        years.style.display = "";
+        icons.style.display = "";
+        arrow.style.transform = "";
+      }
+    });
+    const yearList = document.querySelectorAll(".years li");
+    const dayList = document.querySelectorAll(".days li");
+    dayList.forEach(function (item) {
+      item.addEventListener("click", (e) => {
+        e.stopPropagation();
+        e.target.classList.add("active");
+        currDate = e.target.innerText;
+        checkMonth = currMonth;
+        checkYear = currYear;
+        let monthShow;
+        if (e.target.classList.contains("inactive")) {
+          monthShow =
+            Array.from(dayList).indexOf(e.target) > 20
+              ? currMonth + 1
+              : currMonth - 1;
+        } else {
+          monthShow = currMonth;
+        }
+        timePost.value = `${
+          +(monthShow + 1) < 10 ? "0" + (monthShow + 1) : monthShow + 1
+        }/${+currDate < 10 ? "0" + currDate : currDate}/${currYear}`;
+        dayList.forEach(function (item) {
+          if (item.classList.contains("active") && item !== e.target) {
+            item.classList.remove("active");
+          }
+        });
+      });
+    });
+    yearList.forEach(function (item) {
+      item.addEventListener("click", function (e) {
+        e.stopPropagation();
+        e.target.classList.add("selected");
+        yearList.forEach(function (item) {
+          if (item.classList.contains("selected") && item !== e.target) {
+            item.classList.remove("selected");
+          }
+        });
+        years.style.display = "";
+        icons.style.display = "";
+        arrow.style.transform = "";
+        currYear = +e.target.innerText;
+        checkYear = currYear;
+        let monthShow;
+        if (e.target.classList.contains("inactive")) {
+          monthShow =
+            Array.from(dayList).indexOf(e.target) > 20
+              ? currMonth + 1
+              : currMonth - 1;
+        } else {
+          monthShow = currMonth;
+        }
+        timePost.value = `${
+          +(monthShow + 1) < 10 ? "0" + (monthShow + 1) : monthShow + 1
+        }/${+currDate < 10 ? "0" + currDate : currDate}/${currYear}`;
+        renderCalendar();
+        const dayList = document.querySelectorAll(".days li");
+        dayList.forEach(function (item) {
+          item.addEventListener("click", (e) => {
+            e.stopPropagation();
+            e.target.classList.add("active");
+            currDate = e.target.innerText;
+            checkMonth = currMonth;
+            checkYear = currYear;
+            let monthShow;
+            if (e.target.classList.contains("inactive")) {
+              monthShow =
+                Array.from(dayList).indexOf(e.target) > 20
+                  ? currMonth + 1
+                  : currMonth - 1;
+            } else {
+              monthShow = currMonth;
+            }
+            timePost.value = `${
+              +(monthShow + 1) < 10 ? "0" + (monthShow + 1) : monthShow + 1
+            }/${+currDate < 10 ? "0" + currDate : currDate}/${currYear}`;
+            dayList.forEach(function (item) {
+              if (item.classList.contains("active") && item !== e.target) {
+                item.classList.remove("active");
+              }
+            });
+          });
+        });
+      });
+    });
+  },
+  formatDate: function (time) {
+    const date = new Date(time);
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    return `${hours} giờ ${minutes} phút ngày ${day} tháng ${month + 1} năm ${year}`;
+  },
   start: function () {
     if (blog.isLogin()) {
       client.setToken(localStorage.getItem("access_token"));
@@ -626,9 +920,3 @@ const blog = {
 };
 
 blog.start();
-
-// blog.getProfile().then((user) => {
-//   console.log(user);
-// })
-
-// console.log(blog.getProfile().then((user) => user));
