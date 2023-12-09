95b335d483c8c81bbfd107d44064107f8bc38502
Manh Huy <levi2k3ds@gmail.com>
2023-12-10 01:05:47 +0700

    Update

---
+++ Day_48/portfolio/src/app/[locale]/vi/page.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "@/components/Header/Header";
+import Portfolio from "@/components/Portfolio/Portfolio";
+
+function ViPage() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Portfolio />
+      </main>
+    </>
+  );
+}
+
+export default ViPage;
